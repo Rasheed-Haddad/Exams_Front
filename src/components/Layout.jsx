176715fc3005d7b1cd028e7b03f2e3082c6395f8
Rasheed963 <rasheedhaddad2018@gmail.com>
2025-12-10 17:03:06 +0300

    F

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet, Routes, Route } from "react-router-dom";
-import Sidebar from "./Sidebar";
+import Sidebar from "./SideBar";
 import Header from "./Header";
 import { useSelector } from "react-redux";
 import Admin from "./admin";
